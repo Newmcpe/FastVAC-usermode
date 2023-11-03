a086fd455ee3390c49aff2488497ed6a3a9b1ff7
Alina Chebakova <a.chebakova@suppy.org>
2023-11-03 23:32:30 +0800

    wip

--- 02-hello-world/src/lib.rs
+++ 02-hello-world/src/lib.rs
@@ -1,28 +1,71 @@
 #![no_std]
 
-use core::panic::PanicInfo;
-use winapi::km::wdm::{DbgPrint, DRIVER_OBJECT};
-use winapi::shared::ntdef::{NTSTATUS, UNICODE_STRING};
-use winapi::shared::ntstatus::STATUS_SUCCESS;
+extern crate alloc;
 
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+use alloc::vec;
+use alloc::vec::Vec;
+use windows_kernel_rs::device::{
+    Completion, Device, DeviceDoFlags, DeviceFlags, DeviceOperations, DeviceType, RequestError};
+use windows_kernel_rs::request::{ReadRequest, WriteRequest};
+use windows_kernel_rs::{Access, Driver, Error, kernel_module, KernelModule, SymbolicLink};
+
+struct MyDevice {
+    data: Vec<u8>,
 }
 
-#[no_mangle]
-pub extern "system" fn driver_entry(driver: &mut DRIVER_OBJECT, _: &UNICODE_STRING) -> NTSTATUS {
-    unsafe {
-        DbgPrint("Hello, world!\0".as_ptr());
+impl DeviceOperations for MyDevice {
+    fn read(&mut self, _device: &Device, request: ReadRequest) -> Result<Completion, RequestError> {
+        let mut user_ptr = request.user_ptr();
+        let slice = user_ptr.as_mut_slice();
+
+        let offset = (request.offset() as usize).min(self.data.len());
+        let size = slice.len().min(self.data.len() - offset);
+
+        slice[0..size].copy_from_slice(&self.data[offset..offset + size]);
+
+        Ok(Completion::Complete(size as u32, request.into()))
     }
 
-    driver.DriverUnload = Some(driver_exit);
+    fn write(&mut self, _device: &Device, request: WriteRequest) -> Result<Completion, RequestError> {
+        let user_ptr = request.user_ptr();
 
-    STATUS_SUCCESS
-}
+        if request.offset() > 0 {
+            return Err(RequestError(Error::END_OF_FILE, request.into()))?;
+        }
 
-pub extern "system" fn driver_exit(_driver: &mut DRIVER_OBJECT) {
-    unsafe {
-        DbgPrint("Bye bye!\0".as_ptr());
+        let slice = user_ptr.as_slice();
+        let size = slice.len().min(4096);
+
+        self.data = slice[0..size].to_vec();
+
+        Ok(Completion::Complete(size as u32, request.into()))
     }
 }
+
+struct Module {
+    _device: Device,
+    _symbolic_link: SymbolicLink,
+}
+
+impl KernelModule for Module {
+    fn init(mut driver: Driver, _: &str) -> Result<Self, Error> {
+        let device = driver.create_device(
+            "\\Device\\Womic",
+            DeviceType::Unknown,
+            DeviceFlags::SECURE_OPEN,
+            DeviceDoFlags::DO_BUFFERED_IO,
+            Access::NonExclusive,
+            MyDevice {
+                data: vec![],
+            },
+        )?;
+        let symbolic_link = SymbolicLink::new("\\??\\Womic", "\\Device\\Womic")?;
+
+        Ok(Module {
+            _device: device,
+            _symbolic_link: symbolic_link,
+        })
+    }
+}
+
+kernel_module!(Module);
